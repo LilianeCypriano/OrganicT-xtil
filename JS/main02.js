daeca19da3d922cec87bd7544c8365b5bf786089
Ayrtonley1402 <ayrtonreisdonascimento@gmail.com>
2024-06-27 17:13:11 -0300

    main atualizada

---
+++ JS/main02.js
@@ -0,0 +1,10 @@
+const clique = () =>{
+    let imagemCurso = document.getElementById("imagem")
+    imagemCurso.style.position = 'fixed'
+    imagemCurso.style.width = '30%'
+    imagemCurso.style.height = '80%'
+    imagemCurso.style.top = '10%'
+    imagemCurso.style.left = '25%'
+    imagemCurso.style.transitionDuration = '8s'
+    imagemCurso.style
+}
