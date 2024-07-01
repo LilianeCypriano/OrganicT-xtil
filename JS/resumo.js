f3529a6f97492a2087af57f1fe069fe919605e32
LilianeCypriano <lilianeecypriano@gmail.om>
2024-07-01 02:37:17 -0300

    teste pedido

---
+++ JS/resumo.js
@@ -0,0 +1,82 @@
+
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+  } else {
+    ready()
+  }
+
+
+
+function ready(){
+    const removeProductButtons = document.getElementsByClassName("remove-product-button")
+for (var i = 0; i < removeProductButtons.length; i++) {
+    removeProductButtons[i].addEventListener("click", function(event) {
+        event.target.parentElement.parentElement.remove()
+        updateTotal()
+    } )} 
+}
+
+ // Mudança valor dos inputs
+ const quantityInputs = document.getElementsByClassName("product-qtd-input")
+ for (var i = 0; i < quantityInputs.length; i++) {
+   quantityInputs[i].addEventListener("change", updateTotal)
+
+
+
+ }
+
+
+    
+
+function updateTotal() {
+    const cartProducts = document.getElementsByClassName("cart-product")
+    totalAmount = 0
+  
+    for (var i = 0; i < cartProducts.length; i++) {
+      const productPrice = cartProducts[i].getElementsByClassName("cart-product-price")[0].innerText.replace("R$", "").replace(",", ".")
+      const productQuantity = cartProducts[i].getElementsByClassName("product-qtd-input")[0].value
+  
+      totalAmount += productPrice * productQuantity
+    }
+    
+    totalAmount = totalAmount.toFixed(2)
+    totalAmount = totalAmount.replace(".", ",")
+    document.querySelector(".cart-total-container span").innerText = "R$" + totalAmount
+  }
+    
+
+
+//   Verificar função
+
+  function addProductToCart(event) {
+    const button = event.target
+    // const productInfos = button.parentElement.parentElement
+    const productName = productInfos.getElementById('descricaoo') [0].innerText
+    const productPrice = productInfos.getElementById('valorr')[0].innerText
+
+
+  
+    let newCartProduct = document.createElement("tr")
+    newCartProduct.classList.add("cart-product")
+  
+    newCartProduct.innerHTML =
+      `
+        <td class="product-identification">
+          <strong class="cart-product-title">${productName}</strong>
+        </td>
+        <td>
+          <span class="cart-product-price">${productPrice}</span>
+        </td>
+        <td>
+          <input type="number" value="1" min="0" class="product-qtd-input">
+          <button type="button" class="remove-product-button">Remover</button>
+        </td>
+      `
+    
+    const tableBody = document.querySelector(".cart-table tbody")
+    tableBody.append(newCartProduct)
+    updateTotal()
+  
+    newCartProduct.getElementsByClassName("remove-product-button")[0].addEventListener("click", removeProduct)
+    newCartProduct.getElementsByClassName("product-qtd-input")[0].addEventListener("change", checkIfInputIsNull)
+  }
--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# OrganicTextil
+OrganicTextil
+![pagina inicial] https://photos.google.com/photo/AF1QipNdR48ce1pTyMhiNt4NlmnPMZFwu8EkTHuWFSJU
+
+[Visite o site] https://lilianecypriano.github.io/OrganicTextil/
+
+Empresa em Desenvolvimento
+Sobre o Projeto
+Projeto desenvolvido durante o curso de Desenvolvimento Web oferecido pelo Instituto da Oportunidade Social.
+
+Ferramentas Utilizadas
+Tecnológicas:
+HTML
+CSS
+JavaScript
+Github
+Design:
+Figma
+Canva
+Utilitários (Opcional)
+[Link Figma] https://www.figma.com/design/uNFtALDJBR1wPeOjOztnhW/Untitled?node-id=84-3&t=y3FD1Y3liDOKsEYt-0
+
+[Link Canva] https://www.canva.com/design/DAGG2G_b6h4/mMBxRmPhrtG7HZ8y_BBAWw/edit
+
+Integrantes
+![Liliane Cypriano] https://photos.google.com/search/_tra_/photo/AF1QipMobdH2gfD1COuP-Goa9-npPBQfGeImVV_Z9tgj
+
+GitHub
+
+
+![Ayrton Nascimento] https://photos.google.com/search/_tra_/photo/AF1QipNJu0knd8qsIufdqGhcKAShR5hsc1yaJ-agfOFs
+
+GitHub
+LinkedIn
+![Lucas Pereira] https://photos.google.com/search/_tra_/photo/AF1QipMVe5ZF1jjrzSDfSnkWuucAMQImJ3RhbP4TBuiz
+
+GitHub
+
+LinkedIn
+
+![Jonatas Alencar] https://photos.google.com/search/_tra_/photo/AF1QipM4D_QAFL2jutY-3LMXylVbjjp3EquSfU3FHPWp
+
+GitHub
+
+LinkedIn
+
+Ana Camylla
+
+GitHub
+
+LinkedIn
